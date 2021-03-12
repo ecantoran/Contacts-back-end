397f8d82952e9a1b931d364d666f78ca2f56a1c4
Eduardo Cantoran <ecantoranflores@gmail.com>
2021-03-12 02:24:00 -0600

    [Feat] CRUD contacts api

--- app/src/routes/contacts.js
+++ app/src/routes/contacts.js
@@ -2,8 +2,11 @@ const { Router } = require('express');
 const router = Router();
 const { getContacts, createContact, deleteContact, updateContact } = require('../controllers/index');
 
-router.get('/list', getContacts);
+router.get('/', getContacts);
 
-router.post('/create', createContact);
+router.post('/', createContact);
+
+router.put('/', updateContact);
+router.delete('/', deleteContact)
 
 module.exports = router;
